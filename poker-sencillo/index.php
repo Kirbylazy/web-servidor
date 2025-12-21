e849510c0dd108ad1158e8d1cec0691abb4c253a
Dario Aguilar Rodriguez <darioaguilarrodriguez88@gmail.com>
2025-12-21 22:34:16 +0100

    prueba 7

--- poker-sencillo/index.php
+++ poker-sencillo/index.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Poker de pruebas 6</title>
+    <title>Poker de pruebas 7</title>
 </head>
 <body>
 
