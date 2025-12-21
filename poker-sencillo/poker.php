5675694a94eaf7d8091cb60afdbef3751e6206f8
Dario Aguilar Rodriguez <darioaguilarrodriguez88@gmail.com>
2025-12-21 23:19:09 +0100

    Mejorando los nombres de jugador

--- poker-sencillo/poker.php
+++ poker-sencillo/poker.php
@@ -104,7 +104,7 @@ if (!isset($_SESSION['comenzar']) && !isset($_SESSION['confirmar']) && !isset($_
         <?php foreach ($_SESSION['juego'] as $jugador => $mano): ?>
 
             <tr>
-                <th>Jugador <?= is_string($jugador) ? $jugador : $jugador + 1 ?></th>
+                <th>Jugador <?= is_string($jugador) ? $jugador : $jugador?></th>
                 <th>1</th><th>2</th><th>3</th><th>4</th><th>5</th>
             </tr>
 
