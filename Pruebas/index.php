8c8e78047d30b744c18e9727ba9429e99f6aad34
Dario Aguilar Rodriguez <darioaguilarrodriguez88@gmail.com>
2025-12-21 15:09:57 +0100

    prueba de upload

--- Pruebas/index.php
+++ Pruebas/index.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Poker</title>
+    <title>Poker prueba de upload</title>
 </head>
 <body>
 
